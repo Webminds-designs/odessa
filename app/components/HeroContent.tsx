d47741a02abd5b8add5fa35da0df2933c35fbd8e
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2025-03-04 00:08:37 +0530

    feat/ hero section HeroContent feat/ button hover animation 	new file:   app/asset/Images/ODESSA2.png 	new file:   app/components/HeroContent.tsx

---
+++ app/components/HeroContent.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import React from "react";
+import logo from "../asset/Images/ODESSA2.png";
+
+const HeroContent = () => {
+  return (
+    <div className=" absolute bottom-0 w-full h-screen md:px-24">
+      <div className="flex flex-col items-start justify-center w-full md:w-2/3 h-screen text-start text-white gap-16">
+        <Image src={logo} alt="logo" width={100} height={100} />
+        <div className="flex flex-col gap-2">
+          <div className="text-lg w-2/3 font-aeonikregularitalic ">
+            Our young and expert design the most exquisite jewelry for you to
+            shine in a special way in the world
+          </div>
+
+          <div className="text-4xl md:text-5xl lg:text-7xl font-vasion">
+            Diamonds that reflect your uniqueness.
+          </div>
+        </div>
+        <div className="w-fit h-fit flex gap-0 font-aeonikregularitalic">
+          <div className="group relative inline-block">
+            <div className="relative px-4 py-2 bg-white text-black rounded-full cursor-pointer overflow-hidden  border-white border-2 ">
+              {/* Animated overlay */}
+              <span className="absolute inset-0 bg-[#A07648] transform -translate-x-full transition-transform duration-600 group-hover:translate-x-0"></span>
+              {/* Button text */}
+              <span className="relative z-10 transition-colors duration-600 group-hover:text-white ">
+                Order Now
+              </span>
+            </div>
+          </div>
+          <div className="group relative inline-block">
+            <div className="relative px-4 py-2 bg-transparent text-white border-solid border-white border-2 rounded-full cursor-pointer overflow-hidden  ">
+              {/* Animated overlay */}
+              <span className="absolute inset-0 bg-[#A07648] transform -translate-x-full transition-transform duration-600 group-hover:translate-x-0"></span>
+              {/* Button text */}
+              <span className="relative z-10 transition-colors duration-600 group-hover:text-white ">
+                See Collection
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroContent;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
+import HeroContent from "./components/HeroContent";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Hero />
       {/* extra div   */}
+      <HeroContent />
       <div className="w-full h-screen"></div>
       <div className="w-full h-screen"></div>
     </div>
