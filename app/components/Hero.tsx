13ff3fef6065b0ecdf7e6b7cbfbbcb9c71108f9b
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2025-03-03 13:00:16 +0530

    compontnt folder
    	new file:   app/components/Hero.tsx

---
+++ app/components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center gap-4 bg-amber-200 text-7xl text-amber-800">
+        hERO HERE
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
