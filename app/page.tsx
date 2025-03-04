3b8729fd8e0ceaa7d8c8a78163b8d9f5b274d292
chathuwa-whiz <navodchathushka@gmail.com>
2025-03-04 08:24:13 +0530

    feat: add HeroContent component to Home page and remove redundant divs

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,9 @@ export default function Home() {
   return (
     <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-30 sm:p-20">
       <Hero />
+      <HeroContent />
       <Highlight />
       <ShopByShape />
-      {/* extra div   */}
-      <HeroContent />
-      <div className="w-full h-screen"></div>
-      <div className="w-full h-screen"></div>
     </div>
   );
 }
