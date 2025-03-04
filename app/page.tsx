14212ee5f2489e26e694a9e03089797a22f61e00
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2025-03-04 14:05:56 +0530

    fix / hero overlapping 	modified:   app/components/Hero.tsx 	modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,9 @@ export default function Home() {
   return (
     <div className="grid items-center justify-items-center min-h-screen p-8 pb-20 gap-30 sm:p-20">
       <Hero />
+
       <HeroContent />
+      <div className="w-full h-screen"></div>
       <Highlight />
       <ShopByShape />
     </div>
