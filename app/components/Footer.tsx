d5c2a49b425852cb0b199dd5bb20281dc7a6039c
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2025-03-04 13:59:24 +0530

    feat / footer.tsx

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-screen h-11/12 md:p-24 p-8 text-white flex flex-col items-center justify-center">
+      <div className="bg-teal-200 w-full h-full"></div>
+    </div>
+  );
+};
+
+export default Footer;
