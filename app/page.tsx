1904f19196ef3787289c9be1d0dbfcbd3dc61e6a
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2025-03-07 03:22:34 +0530

    feature / header

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,14 @@ import Footer from "./components/Footer";
 import Contact from "./components/Contanct";
 import NewCollection from "./components/NewCollection";
 import dynamic from "next/dynamic";
+import NavBar from "./components/NavBar";
 
 const Hero = dynamic(() => import("./components/Hero"), { ssr: false });
 
 export default function Home() {
   return (
     <div>
+      <NavBar />
       <Hero />
       <HeroContent />
       <div className="w-full h-screen"></div>
