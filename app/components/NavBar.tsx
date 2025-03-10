85ce701b326ea486c40e8e5205bdddd2e5d889f4
chathuwa-whiz <navodchathushka@gmail.com>
2025-03-10 19:19:08 +0530

    add smooth scrolling and update contact link for better navigation

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -51,7 +51,7 @@ export default function Header() {
               </Link>
             </li>
             <li>
-              <Link href="/contact" className="cursor-pointer">
+              <Link href="#contact" className="cursor-pointer">
                 Contact Us
               </Link>
             </li>
